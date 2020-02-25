09ecf96ad4d6314f4701ef725eda13cf2ceb9aa2
Smith, Geoffrey <SMITHG50@nationwide.com>
2020-02-25 14:12:46 -0500

    Refactoing: decided adding a new Item class was over complicating things and made a new change.

--- lib/pos_system.rb
+++ lib/pos_system.rb
@@ -1,19 +1,19 @@
 class POSSystem
 
-  attr_accessor :prices
+  attr_accessor :items
 
   def initialize
-    @prices = {
-        soup: Item.new('soup', 1.89),
-        crackers: Item.new('crackers', 1.99)
+    @items = {
+        soup:     { price: 1.89 },
+        crackers: { price: 1.99 }
     }
   end
 
   def cost(item_name)
-    @prices[item_name.to_sym].price
+    @items[item_name.to_sym][:price]
   end
 
   def set_cost(item_name, price)
-    @prices[item_name.to_sym] = Item.new item_name, price
+    @items[item_name.to_sym] = { price: price }
   end
 end
