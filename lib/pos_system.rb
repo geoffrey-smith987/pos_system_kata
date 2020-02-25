3edb97dfb01840ef846ec285ece06d886fddf5d9
Smith, Geoffrey <SMITHG50@nationwide.com>
2020-02-25 14:03:33 -0500

    Refactoring: updated the pos_system and spec to use the new item class.

--- lib/pos_system.rb
+++ lib/pos_system.rb
@@ -4,16 +4,16 @@ class POSSystem
 
   def initialize
     @prices = {
-        soup: 1.89,
-        crackers: 1.99
+        soup: Item.new('soup', 1.89),
+        crackers: Item.new('crackers', 1.99)
     }
   end
 
-  def cost(item)
-    @prices[item]
+  def cost(item_name)
+    @prices[item_name.to_sym].price
   end
 
-  def set_cost(item, price)
-    @prices[item] = price
+  def set_cost(item_name, price)
+    @prices[item_name.to_sym] = Item.new item_name, price
   end
 end
--- spec/pos_system_spec.rb
+++ spec/pos_system_spec.rb
@@ -3,13 +3,13 @@ require_relative '../lib/pos_system'
 describe POSSystem do
   it 'should return the cost of an item' do
     system = POSSystem.new
-    expect(system.cost(:soup)). to eq 1.89
-    expect(system.cost(:crackers)). to eq 1.99
+    expect(system.cost('soup')). to eq 1.89
+    expect(system.cost('crackers')). to eq 1.99
   end
 
   it 'should allow you to set the cost of an item' do
     system = POSSystem.new
-    system.set_cost(:soup, 1.00)
-    expect(system.cost(:soup)). to eq 1.00
+    system.set_cost('soup', 1.00)
+    expect(system.cost('soup')). to eq 1.00
   end
 end
